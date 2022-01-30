a64dd2cab1c6173a400bcaff60320c294892de42
Suat SATILMIŞ <suatstlm41@gmail.com>
2022-01-31 00:15:03 +0300

    Add project files.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database1 = new SqlServer();
+            database1.Add();
+            database1.Delete();
+
+            Database database2 = new Oracle();
+            database2.Add();
+            database2.Delete();
+        }
+    }
+    
+
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public abstract void Delete();
+    }
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Sql");
+        }
+    }
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+
+}
